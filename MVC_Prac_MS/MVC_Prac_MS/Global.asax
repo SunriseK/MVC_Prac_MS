b000cab13a3c5d79a19f2d52829cf351a8b33d92
SunriseK <mrkao7@gmail.com>
2018-08-02 15:03:08 +0800

    Commit test

---
+++ MVC_Prac_MS/MVC_Prac_MS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Prac_MS.MvcApplication" Language="C#" %>
---
+++ MVC_Prac_MS/MVC_Prac_MS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Prac_MS.Startup))]
+namespace MVC_Prac_MS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
